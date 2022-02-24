20d24a27d2af8f9ab3360546007f8950cbf5f4db
alexandre santos grisende <alexandrehgrisende@hotmail.com>
2022-02-23 23:59:34 -0300

    finished test

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,57 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+
+        $segmentations = [];
+        $preto = [];
+        $branco = [];
+        $azul = [];
+        $vermelho = [];
+
+        foreach (self::products as $product)
+        {
+            array_push($segmentations, explode('-', $product));
+        }
+
+        foreach ($segmentations as $segmentation)
+        {
+            if ($segmentation[0] == 'preto')
+            {
+                $c = ['preto' => $segmentation[1]];
+                array_push($preto, $c['preto']);
+            }
+
+            if ($segmentation[0] == 'branco')
+            {
+                $c = ['branco' => $segmentation[1]];
+                array_push($branco, $c['branco']);
+            }
+
+            if ($segmentation[0] == 'vermelho')
+            {
+                $c = ['vermelho' => $segmentation[1]];
+                array_push($vermelho, $c['vermelho']);
+            }
+
+            if ($segmentation[0] == 'azul')
+            {
+                $c = ['azul' => $segmentation[1]];
+                array_push($azul, $c['azul']);
+            }
+        }
+
+        $p = array_count_values($preto);
+        $b = array_count_values($branco);
+        $a = array_count_values($azul);
+        $v = array_count_values($vermelho);
+
+        $colors = [
+            'preto'    => $p,
+            'branco'   => $b,
+            'vermelho' => $v,
+            'azul'     => $a
+        ];
+
+        return $colors;
     }
 }
